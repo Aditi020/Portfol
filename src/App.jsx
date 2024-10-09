49440791acc3ae20db339449534ea41d53bd7a6f
Aditi020 <aditi.kumar.2.2.24@gmail.com>
2024-10-09 20:04:55 +0530

    Working with Routing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import Sidebar from './Components/Sidebar.jsx';
-// import Navbar from './Components/Navbar';
-// import About from './Components/About';
-// import Resume from './Components/Resume';
-// import Portfolio from './Components/Portfolio';
-// import Blog from './Components/Blog';
-// import Contact from './Components/Contact';
+
 import Layout from './Layout';
 
 function App() {
